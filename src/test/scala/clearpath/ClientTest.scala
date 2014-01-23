304d74a066a0ab895ef607ced58b53d7f1a3007d
mmcdon20 <mmcdon20@live.com>
2014-01-22 19:36:13 -0600

    Added tests for community events

--- src/test/scala/clearpath/ClientTest.scala
+++ src/test/scala/clearpath/ClientTest.scala
@@ -54,4 +54,43 @@ class ClientTest extends FunSpec {
       assert(sort != unsorted)
     }
   }
+
+  describe("Community Events Method"){
+    it("should return a list of CommunityEvents") {
+      val eventsFuture = client.communityEvents()
+      val result: List[CommunityEvent] = Await.result(eventsFuture, timeLimit)
+      assert(!result.isEmpty)
+    }
+    it("should be able to set a max limit") {
+      val eventFuture1 = client.communityEvents(max = 5)
+      val eventFuture2 = client.communityEvents(max = 1)
+      val result1: List[CommunityEvent] = Await.result(eventFuture1, timeLimit)
+      val result2: List[CommunityEvent] = Await.result(eventFuture2, timeLimit)
+      assert(result1.length == 5)
+      assert(result2.length == 1)
+    }
+    it("should be able to order results in asc and desc order") {
+      val ascFuture  = client.communityEvents(order = "asc")
+      val descFuture = client.communityEvents(order = "desc")
+      val asc:  List[CommunityEvent] = Await.result(ascFuture, timeLimit)
+      val desc: List[CommunityEvent] = Await.result(descFuture, timeLimit)
+      assert(asc != desc)
+    }
+    it("Should be able to set an offset"){
+      val offset1Future = client.communityEvents(offset = 1)
+      val offset2Future = client.communityEvents(offset = 2)
+      val off1: List[CommunityEvent] = Await.result(offset1Future, timeLimit)
+      val off2: List[CommunityEvent] = Await.result(offset2Future, timeLimit)
+      assert(off1(1) == off2(0))
+      assert(off1 != off2)
+    }
+    it("Should be able to sort on a field name"){
+      val sortFuture     = client.communityEvents(sort = "location")
+      val unsortedFuture = client.communityEvents()
+      val sort:     List[CommunityEvent] = Await.result(sortFuture, timeLimit)
+      val unsorted: List[CommunityEvent] = Await.result(unsortedFuture, timeLimit)
+      assert(sort == sort.sortWith(_.location.getOrElse("") < _.location.getOrElse("")))
+      assert(sort != unsorted)
+    }
+  }
 }
