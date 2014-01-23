b9ed0e81c0d6c1186621d42fe905689b939ed4c1
mmcdon20 <mmcdon20@live.com>
2014-01-23 03:13:20 -0600

    Refactored url strings in client

--- src/main/scala/clearpath/ClearpathClient.scala
+++ src/main/scala/clearpath/ClearpathClient.scala
@@ -12,7 +12,7 @@ import CrimeJsonProtocol._
 
 class ClearpathClient(implicit system: ActorSystem) {
   import system.dispatcher
-  private val base = "http://api1.chicagopolice.org"
+  private val base = "http://api1.chicagopolice.org/clearpath/api/1.0/"
 
   private def get [T](method: String)(arguments: String)(implicit f: JsonFormat[T]): Future[List[T]] = {
     val pipeline = sendReceive ~> unmarshal[List[T]]
@@ -22,47 +22,47 @@ class ClearpathClient(implicit system: ActorSystem) {
 
   def mostWanted(effectiveDate: String="", district: String="", max: Int=10, sort: String="", offset: Int=0) = get
     [ WantedCriminal ]
-    { "/clearpath/api/1.0/mostWanted/list" }
+    { "mostWanted/list" }
     { s"?effectiveDate=$effectiveDate&district=$district&max=$max&sort=$sort&offset=$offset" }
 
   def communityEvents(max: Int=10, sort: String="", offset: Int=0) = get
     [ CommunityEvent ]
-    { "/clearpath/api/1.0/communityCalendar/events" }
+    { "communityCalendar/events" }
     { s"?max=$max&sort=$sort&offset=$offset" }
 
   def communityCalendars(max: Int=10, sort: String="", offset: Int=0) = get
     [ CommunityCalendar ]
-    { "/clearpath/api/1.0/communityCalendar" }
+    { "communityCalendar" }
     { s"?max=$max&sort=$sort&offset=$offset" }
 
   def crimesMajor(block: String="", max: Int=10, sort: String="", offset: Int=0) = get
     [ Crime ]
-    { "/clearpath/api/1.0/crimes/major" }
+    { "crimes/major" }
     { s"?block=$block&max=$max&sort=$sort&offset=$offset" }
 
   def crimesNearby(x: Int, y: Int, radius: Int=1000, max: Int=10, sort: String="", offset: Int=0) = get
     [ Crime ]
-    { "/clearpath/api/1.0/crimes/nearbyXY" }
+    { "crimes/nearbyXY" }
     { s"?x=$x&y=$y&radius=$radius&max=$max&sort=$sort&offset=$offset" }
 
   def crimesRdNo(rdNo: Int, max: Int=10, sort: String="", offset: Int=0) = get
     [ Crime ]
-    { "/clearpath/api/1.0/crimes/rdNo" }
+    { "crimes/rdNo" }
     { s"?rdNo=$rdNo&max=$max&sort=$sort&offset=$offset" }
 
   def crimesType(primary: String, max: Int=10, sort: String="", offset: Int=0) = get
     [ Crime ]
-    { "/clearpath/api/1.0/crimes/type" }
+    { "crimes/type" }
     { s"?primary=$primary&max=$max&sort=$sort&offset=$offset" }
 
   def crimesList(max: Int=10, sort: String="", offset: Int=0) = get
     [ Crime ]
-    { "/clearpath/api/1.0/crimes/list" }
+    { "crimes/list" }
     { s"?max=$max&sort=$sort&offset=$offset" }
 
   def mugshots(warrantNo: String) = get
     [ Mugshot ]
-    { "/clearpath/api/1.0/mugshots" }
+    { "mugshots" }
     { s"?warrantNo=$warrantNo" }
 
 }
